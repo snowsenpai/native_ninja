8e5244aea09044f210a53af323da1b9c8fc010ce
Umoh Samuel <samuelwisdom360@gmail.com>
2025-12-23 20:53:22 +0100

    working with themes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,23 +1,32 @@
 import { Stack } from 'expo-router'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, useColorScheme } from 'react-native'
+import { Colors } from '../constants/Colors';
+import { StatusBar } from 'expo-status-bar';
 
 const RootLayout = () => {
-  return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#f18565ff',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-      <Stack.Screen name="index" options={{ title: 'Home', headerShown: false }} />
-      <Stack.Screen name="about" options={{ title: 'About' }} />
-      <Stack.Screen name="contact" options={{ title: 'Contact' }} />
-    </Stack>
-  );
+    const colorScheme = useColorScheme();
+
+    const theme = Colors[colorScheme] ?? Colors.light;
+
+    return (
+        <>
+            <StatusBar value="auto" />
+            <Stack
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: theme.navBackground,
+                    },
+                    headerTintColor: theme.title,
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    },
+                }}>
+                <Stack.Screen name="index" options={{ title: 'Home', headerShown: false }} />
+                <Stack.Screen name="about" options={{ title: 'About' }} />
+                <Stack.Screen name="contact" options={{ title: 'Contact' }} />
+            </Stack>
+        </>
+    );
 }
 
 export default RootLayout
