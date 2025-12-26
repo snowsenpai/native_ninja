da69e8ca971b2abe1ae9ce2c57eaba280073c1fd
Umoh Samuel <samuelwisdom360@gmail.com>
2025-12-26 14:00:51 +0100

    add dashboard layout with tab navigation and themed screens for profile, create, and books

---
+++ app/(dashboard)/_layout.jsx
@@ -0,0 +1,53 @@
+import { Tabs } from 'expo-router'
+import { useColorScheme } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { Colors } from '../../constants/Colors'
+
+const DashboardLayout = () => {
+    const colorScheme = useColorScheme();
+    const theme = Colors[colorScheme] ?? Colors.light;
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: theme.navBackground,
+                    paddingTop: 10,
+                    height: 90,
+                },
+                tabBarActiveTintColor: theme.iconColorFocused,
+                tabBarInactiveTintColor: theme.iconColor,
+            }}
+        >
+            <Tabs.Screen
+                name="profile"
+                options={{ title: 'Profile', tabBarIcon: ({ focused }) => (
+                    <Ionicons
+                        name={focused ? "person" : "person-outline"}
+                        size={24}
+                        color={focused ? theme.iconColorFocused : theme.iconColor}
+                    />) }}
+            />
+            <Tabs.Screen
+                name="create"
+                options={{ title: 'Create', tabBarIcon: ({ focused }) => (
+                    <Ionicons
+                        name={focused ? "add-circle" : "add"}
+                        size={24}
+                        color={focused ? theme.iconColorFocused : theme.iconColor}
+                    />) }}
+            />
+            <Tabs.Screen
+                name="books"
+                options={{ title: 'Books', tabBarIcon: ({ focused }) => (
+                    <Ionicons
+                        name={focused ? "book" : "book-outline"}
+                        size={24}
+                        color={focused ? theme.iconColorFocused : theme.iconColor}
+                    />) }}
+            />
+        </Tabs>
+    )
+}
+
+export default DashboardLayout
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -23,6 +23,7 @@ const RootLayout = () => {
                 }}>
 
                 <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+                <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
                 <Stack.Screen name="index" options={{ title: 'Home', headerShown: false }} />
                 <Stack.Screen name="about" options={{ title: 'About' }} />
                 <Stack.Screen name="contact" options={{ title: 'Contact' }} />
